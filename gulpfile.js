466df84e9b75d7b93773f9327233a142b3ede299
Chris Cornwell <chris@citizennet.com>
2017-11-09 16:34:34 -0800

    chore: add testing tasks for gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ var concat        = require('gulp-concat');
 var rename        = require('gulp-rename');
 var uglify        = require('gulp-uglify');
 var templateCache = require('gulp-angular-templatecache');
+var tape          = require('gulp-tape');
+var tapColorize   = require('tap-colorize');
+var spawn         = require('child_process').spawn;
 
 // Lint Task
 gulp.task('lint', function() {
@@ -21,6 +24,42 @@ gulp.task('lint', function() {
       }))
       .pipe(jshint.reporter('default'));
 });
+
+// Run Test
+gulp.task('test', (done) => {
+  return gulp
+  .src('tests/output/test.bundle.js')
+  .pipe(tape({
+    reporter: tapColorize()
+  }));
+});
+
+const setupWatch = watch => {
+  watch.stdout.on('data',
+    data => console.log(`webpack stdout: ${data}`));
+  watch.stderr.on('data',
+    data => console.log(`webpack stderr: ${data}`));
+  watch.on('close',
+    code => console.log(`webpack process exited with code ${code}`));
+  return watch;
+}
+
+const runWebpack = (params=[]) =>
+  setupWatch(spawn(`${bin}/webpack`, params))
+
+const testConfig = 'tests/webpack.test.config.js';
+
+gulp.task('webpack:dev', () =>
+  runWebpack(['--color'])
+);
+
+gulp.task('webpack:test', () =>
+  runWebpack([
+    '--color',
+    '--config',
+    testConfig
+  ])
+);
 
 // Add templates to templateCache
 gulp.task('templates', function() {
@@ -48,6 +87,8 @@ gulp.task('scripts', function () {
 // Watch Files For Changes
 gulp.task('watch', function() {
   gulp.watch(['src/templates/*.html', 'src/*.js'], ['lint', 'templates', 'scripts']);
+  gulp.watch(['src/*.spec.js'], ['webpack:test']);
+  gulp.watch(['tests/output/test.bundle.js'], ['test']);
 });
 
 // Build Task
